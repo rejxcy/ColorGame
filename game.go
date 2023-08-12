af2260463ae2597ba3baec534855b341a760fcbc
rejxcy <rejxcy0903@gmail.com>
2023-08-12 18:55:45 +0800

    實作 遊戲相關邏輯

---
+++ game.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"math/rand"
+)
+
+type Game struct {
+	isGameEnd  bool
+	quizList   []string
+	colorList  []string
+	whichQuiz  int
+	wrongCount int
+}
+
+func NewGame() *Game {
+
+	return &Game{
+		isGameEnd:  false,
+		quizList:   randomColors(10),
+		colorList:  randomColors(10),
+		whichQuiz:  0,
+		wrongCount: 0,
+	}
+}
+
+func (g *Game) getQuiz() (string, string) {
+	if g.whichQuiz >= len(g.quizList) {
+		g.isGameEnd = true
+		return "", ""
+	}
+
+	quiz := g.quizList[g.whichQuiz]
+	color := g.colorList[g.whichQuiz]
+
+	return quiz, color
+}
+
+func (g *Game) isAnswer(color string) bool {
+	if color == g.quizList[g.whichQuiz] {
+		g.whichQuiz++
+		return true
+	}
+	
+	g.wrongCount ++
+	return false
+}
+
+func randomColors(count int) []string {
+	colors := []string{"red", "green", "blue", "yellow", "orange", "purple"}
+
+	result := make([]string, count)
+	for i := 0; i < count; i++ {
+		randomIndex := rand.Intn(len(colors))
+		result[i] = colors[randomIndex]
+	}
+
+	return result
+}
--- ws.go
+++ ws.go
@@ -14,53 +14,55 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-var questions = []string{"紅色", "綠色", "藍色", "黃色", "橘色", "紫色"}
-var colors = []string{"red", "green", "blue", "yellow", "orange", "purple"}
-
 var clients = make(map[string]*websocket.Conn)
-var questionCount = 0
+var game *Game = NewGame()
 
 func WS(ctx *gin.Context) {
-
-	conn, err := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
+	ws, err := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer conn.Close()
+	defer ws.Close()
 
 	client := ctx.Param("client")
-	
-	// TODO: key check
-	clients[client] = conn
+
+	clients[client] = ws
 	defer delete(clients, client)
 
-	sendQuestion(conn)
+	sendQuestion(ws)
 
 	for {
-		_, m, err := conn.ReadMessage()
+		_, m, err := ws.ReadMessage()
 		if err != nil {
 			fmt.Println("Error with read message:", err)
 			return
 		}
-		
-		if string(m) == colors[questionCount] {
-			NewQuestion()
-			for _, w := range clients {
-                sendQuestion(w)
-            }
-		} 
+
+		if game.isAnswer(string(m)) {
+			if !game.isGameEnd {
+				sendQuestionToAll()
+			}
+		}
+
+		if game.isGameEnd {
+			fmt.Println("game is over!!")
+		}
 	}
 }
 
-func sendQuestion(conn *websocket.Conn) {
-	message := fmt.Sprintf("%s,%s", questions[questionCount], colors[questionCount])
-	if err := conn.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
-		fmt.Println("Error sending question:", err)
+func sendQuestionToAll() {
+	for _, ws := range clients {
+		sendQuestion(ws)
 	}
 }
 
-func NewQuestion() {
-	//TODO: game logic
-	questionCount ++
+func sendQuestion(ws *websocket.Conn) {
+	quiz, color := game.getQuiz()
+	if quiz != "" && color != "" {
+		message := fmt.Sprintf("%s,%s", quiz, color)
+		if err := ws.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
+			fmt.Println("Error sending question:", err)
+		}
+	}
 }
