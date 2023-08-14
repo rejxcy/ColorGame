82d576af493f504ad1c6e91b38cc1bc8cd65f21d
rejxcy <rejxcy0903@gmail.com>
2023-08-14 21:20:44 +0800

    調整 首頁與其他頁面文字內容

--- main.go
+++ main.go
@@ -10,17 +10,19 @@ import (
 
 func main() {
 	router := gin.Default()
-
 	router.LoadHTMLGlob("client/*")
-	fmt.Println("Server Start!")
-	router.GET("/lobby", Lobby)
-	router.GET("/game_entry", GameEntry)
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "home.html", nil)
+	})
 	router.GET("/player_name", func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "player_name.html", nil)
 	})
-	router.POST("/player_view", PlayerView)
+	router.GET("/game_entry", GameEntry)
 	router.GET("/game_view", GameView)
 	router.GET("/ws/:client", WS)
+
+	router.POST("/player_view", PlayerView)
 
 	router.Run()
 }
